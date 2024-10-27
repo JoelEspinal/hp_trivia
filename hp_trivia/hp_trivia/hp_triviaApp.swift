de66fe603cbf7f2328954297ec5422bb41aff091
JoelEspinal <j.espinalcarrasco@gmail.com>
2024-10-27 18:15:18 -0400

    Create project

---
+++ hp_trivia/hp_trivia/hp_triviaApp.swift
@@ -0,0 +1,20 @@
+//
+//  hp_triviaApp.swift
+//  hp_trivia
+//
+//  Created by Joel Espinal on 27/10/24.
+//
+
+import SwiftUI
+
+@main
+struct hp_triviaApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ hp_trivia/hp_triviaTests/hp_triviaTests.swift
@@ -0,0 +1,17 @@
+//
+//  hp_triviaTests.swift
+//  hp_triviaTests
+//
+//  Created by Joel Espinal on 27/10/24.
+//
+
+import Testing
+@testable import hp_trivia
+
+struct hp_triviaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
