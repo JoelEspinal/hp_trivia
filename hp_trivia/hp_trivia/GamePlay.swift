74b0baa40f08d275bca503d233d70ed87bff93b9
JoelEspinal <j.espinalcarrasco@gmail.com>
2024-11-15 11:57:11 -0400

    Fix Image error
    :q

---
+++ hp_trivia/hp_trivia/GamePlay.swift
@@ -0,0 +1,31 @@
+//
+//  GamePlay.swift
+//  hp_trivia
+//
+//  Created by Joel Espinal on 15/11/24.
+//
+
+import Foundation
+import SwiftUI
+
+
+
+struct GamePlay: View {
+    var body: some View {
+        GeometryReader { geo in
+            ZStack {
+                Image("hogwarts")
+                    .renderingMode(.none)
+                    .resizable()
+                    .frame(width: geo.size.width * 3, height: geo.size.height * 1.05)
+            }
+            .frame(width: geo.size.width, height: geo.size.height)
+        }.ignoresSafeArea()
+    }
+}
+
+struct GamePlay_Previews: PreviewProvider {
+    static var previews: some View {
+        GamePlay()
+    }
+}
