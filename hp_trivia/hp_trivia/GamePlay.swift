b7bb316cff9f5be4017e6c8de4637c2007f8cd7d
JoelEspinal <j.espinalcarrasco@gmail.com>
2024-11-15 12:21:01 -0400

    Indent Game Play

--- hp_trivia/hp_trivia/GamePlay.swift
+++ hp_trivia/hp_trivia/GamePlay.swift
@@ -11,35 +11,38 @@ import SwiftUI
 
 
 struct GamePlay: View {
-    var body: some View {
-        GeometryReader { geo in
-            ZStack {
-                Image("hogwarts")
-                    .renderingMode(.none)
-                    .resizable()
-                    .frame(width: geo.size.width * 3, height: geo.size.height * 1.05)
-                   .overlay(Rectangle().foregroundColor(Color.black.opacity(0.8)))
-                HStack {
-                    Button("End Game") {
-                        // TODO: End Game
-                    }
-                    .buttonStyle(.borderedProminent)
-                    .tint(.red.opacity(0.5))
-                    
-                    Spacer()
-                    
-                    
+var body: some View {
+    GeometryReader { geo in
+        ZStack {
+            Image("hogwarts")
+                .renderingMode(.none)
+                .resizable()
+                .frame(width: geo.size.width * 3, height: geo.size.height * 1.05)
+                .overlay(Rectangle().foregroundColor(Color.black.opacity(0.8)))
+            VStack {
+            HStack {
+                Button("End Game") {
+                    // TODO: End Game
                 }
-                .frame(width: geo.size.width, height: geo.size.height)
-                .foregroundColor(.white)
+                .buttonStyle(.borderedProminent)
+                .tint(.red.opacity(0.5))
+                
+                Spacer()
+                
+                Text("Score: 33")
+                
             }
+        }
             .frame(width: geo.size.width, height: geo.size.height)
-        }.ignoresSafeArea()
-    }
+            .foregroundColor(.white)
+        }
+        .frame(width: geo.size.width, height: geo.size.height)
+    }.ignoresSafeArea()
+}
 }
 
 struct GamePlay_Previews: PreviewProvider {
-    static var previews: some View {
-        GamePlay()
-    }
+static var previews: some View {
+    GamePlay()
+}
 }
