a0efdc6ffbafd4499c6eda6354ad00c0127b74a8
JoelEspinal <j.espinalcarrasco@gmail.com>
2024-11-15 11:27:08 -0400

    Add functional settings screen

--- hp_trivia/hp_trivia/Settings.swift
+++ hp_trivia/hp_trivia/Settings.swift
@@ -8,81 +8,100 @@
 import Foundation
 import SwiftUI
 
+enum BookStatus {
+    case active
+    case inactive
+    case locked
+}
 
 struct Settings: View {
-    @Environment(\.dismiss) private var dismiss
-    
-    var body: some View {
-        ZStack {
-            InfoBackgroundImage()
-            
-            VStack {
-                
-                Text ("Witch books would you like to see question from?")
-                    .font(.title)
-                    .multilineTextAlignment(.center)
-                ScrollView {
-                    LazyVGrid(columns: [GridItem(), GridItem()]){
-                        ZStack(alignment: .bottomTrailing) {
-                            Image("hp1")
-                                .renderingMode(.none)
-                                .resizable()
-                                .scaledToFit()
-                                .shadow(radius: 7)
-                            
-                            Image(systemName: "checkmark.circle.fill")
-                                .font(.largeTitle)
-                                .imageScale(.large)
-                                .foregroundColor(Color.green)
-                                .shadow(radius: 1)
-                                .padding(3)
+@Environment(\.dismiss) private var dismiss
+
+@State private var books: [BookStatus] = [.active, .active, .inactive,
+                                  .locked, .locked, .locked, .locked]
+
+var body: some View {
+ZStack {
+InfoBackgroundImage()
+
+VStack {
+    Text ("Witch books would you like to see question from?")
+        .font(.title)
+        .multilineTextAlignment(.center)
+        .padding(.top)
+    ScrollView {
+        LazyVGrid(columns: [GridItem(), GridItem()]){
+            ForEach(0 ..< 7) { i in
+                if books[i] == .active {
+                    ZStack(alignment: .bottomTrailing) {
+                        Image("hp\(i+1)")
+                            .renderingMode(.none)
+                            .resizable()
+                            .scaledToFit()
+                            .shadow(radius: 7)
+                        
+                        Image(systemName: "checkmark.circle.fill")
+                            .font(.largeTitle)
+                            .imageScale(.large)
+                            .foregroundColor(Color.green)
+                            .shadow(radius: 1)
+                            .padding(3)
                         }
+                    .onTapGesture {
+                        books[i] = .inactive
+                    }
+                    } else if books[i] == .inactive {
                         ZStack(alignment: .bottomTrailing) {
-                            Image("hp2")
+                            Image("hp\(i+1)")
                                 .renderingMode(.none)
                                 .resizable()
                                 .scaledToFit()
                                 .shadow(radius: 7)
                             
                                 .overlay(Rectangle().opacity(0.33))
-                                
+                            
                             Image(systemName: "circle")
                                 .font(.largeTitle)
                                 .foregroundColor(.green.opacity(0.5))
                                 .shadow(radius: 1)
                                 .padding(3)
-                            
                         }
                         
-                        
+                    .onTapGesture {
+                        books[i] = .active
+                    }
+                    } else {
                         ZStack {
-                            Image("hp3")
+                            Image("hp\(i+1)")
                                 .renderingMode(.none)
                                 .resizable()
                                 .scaledToFit()
                                 .shadow(radius: 7)
                             
                                 .overlay(Rectangle().opacity(0.75))
-                                
+                            
                             Image(systemName: "lock.fill")
                                 .font(.largeTitle)
                                 .imageScale(.large)
                                 .shadow(color: .white.opacity(0.75), radius: 1)
-                               
                         }
                     }
-                    .padding()
                 }
-                
-                Button("Done") {
-                }
-                .doneButton()
             }
         }
     }
+    .padding()
+    }
+    
+    Button("Done") {
+    }
+    .doneButton()
+}
 }
 
+
+
 #Preview {
-    Settings()
+Settings()
 }
-    
+
