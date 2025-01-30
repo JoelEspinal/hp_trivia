c8e9bd4cf6b8100c571441d082e7800da50ed503
JoelEspinal <j.espinalcarrasco@gmail.com>
2025-01-29 22:05:37 -0400

    Add some purchases config

--- hp_trivia/hp_trivia/Store.swift
+++ hp_trivia/hp_trivia/Store.swift
@@ -23,16 +23,66 @@ class Store: ObservableObject {
                                       .locked, .locked, .locked, .locked]
     
     @Published var products: [Product] = []
+    @Published var purchasedIDs = Set<String>()
     
     private var productIDs = ["hp4", "hp5", "hp6", "hp7"]
     
     func loadProducrs() async {
         do {
-            products = try await Product.products(for: productIDs)
-        } catch {
-            print("Couldn't fetch those products: \(error)")
+            let results = try await Product.products(for: productIDs)
         }
+        catch {
+            print("Couldn't fetch those products: \(error)")
             
+        }
+    }
+    
+    func purchase(_ product: Product) async {
+        do {
+            let result = try await product.purchase()
+            
+            switch result {
+            case .success(let verificationResult):
+                switch verificationResult {
+                case .verified:
+                    print("Product purchased!")
+                    
+                case .unverified(let signedType, let verificationError):
+                    print("Error on: \(signedType): \(verificationError)")
+                    
+                case .verified(let signedType):
+                    purchasedIDs.insert(signedType.productID)
+                    
+                }
+            case .userCancelled:
+                break
+                
+            case .pending:
+                break
+            @unknown default:
+                break
+            }
+        } catch {
+            print("Couldn't purchase the product: \(error)")
+        }
+    }
+    
+    func checkProducs() async {
+        for product in products {
+            guard let state = await product.currentEntitlement else { return }
+            
+            switch state {
+                case .unverified(let signedType, let verificationError):
+                    print("Eror on \(signedType): \(verificationError)")
+            case .verified(let singnedType):
+                if singnedType.revocationDate == nil {
+                    purchasedIDs.insert(singnedType.productID)
+                }
+                else {
+                    purchasedIDs.remove(singnedType.productID)
+                }
+            }
+        }
     }
 }
     
